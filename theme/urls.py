947849b91a34c20e109a53d739f6d99278a9bd5a
Krzyszio <krzysztof.maron.1976@gmail.com>
2025-01-05 15:12:28 +0100

    patch

--- theme/urls.py
+++ theme/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('members/', views.get_paid_members, name='get-paid-members'), #GET
     path('members/create', views.create_paid_member, name='create-paid-member'), #POST
-    path('members/delete/<int:pk>/', views.delete_paid_member, name='delete-paid-member'), #DELETE
+    path('members/delete', views.delete_paid_member, name='delete-paid-member'), #DELETE
+    path('members/update', views.patch_paid_member, name='patch-paid-member'), #PATCH
 ]
--- theme/views.py
+++ theme/views.py
@@ -38,12 +38,38 @@ def create_paid_member(request):
 # DELETE View
 @csrf_exempt
 @api_key_required
-def delete_paid_member(request, pk):
+def delete_paid_member(request, email):
     if request.method == "DELETE":
         try:
-            member = PaidMember.objects.get(pk=pk)
+            data = json.loads(request.body)
+            email = data.get('email')
+            if not email:
+                return JsonResponse({"error": "Email is required"}, status = 400)
+            member = PaidMember.objects.get(email=email)
             member.delete()
-            return JsonResponse({"message": f"Member {pk} deleted"}, status = 200)
+            return JsonResponse({"message": f"Member {email} deleted"}, status = 200)
         except PaidMember.DoesNotExist:
             return JsonResponse({"error": "Member not found"}, status = 404)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status = 400)
+    return JsonResponse({"error": "Method not allowed"}, status = 405)
+
+@csrf_exempt
+@api_key_required
+def patch_paid_member(request, email):
+    if request.method == "PATCH":
+        try:
+            data = json.loads(request.body)
+            email = data.get('email')
+            canceled = data.get('canceled')
+            if not email and not canceled:
+                return JsonResponse({"error": "Email and cancel is required"}, status = 400)
+            member = PaidMember.objects.get(email=email)
+            member.canceled = canceled
+            member.save()
+            return JsonResponse({"message": f"Member {email} updated"}, status = 200)
+        except PaidMember.DoesNotExist:
+            return JsonResponse({"error": "Member not found"}, status = 404)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status = 400)
     return JsonResponse({"error": "Method not allowed"}, status = 405)
