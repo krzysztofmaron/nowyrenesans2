c43af16ed99f0d2d3a39e7f1ba9791c9f5ea075b
Krzyszio <95748796+krzysztofmaron@users.noreply.github.com>
2026-05-23 17:24:55 +0200

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 app = get_wsgi_application()
+
+import socket
+
+orig_getaddrinfo = socket.getaddrinfo
+
+def getaddrinfo_ipv4(*args, **kwargs):
+    return [
+        item for item in orig_getaddrinfo(*args, **kwargs)
+        if item[0] == socket.AF_INET
+    ]
+
+socket.getaddrinfo = getaddrinfo_ipv4
