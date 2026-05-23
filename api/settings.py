774a55d9455248b6c0ed2e782b4d3cc81a36c48c
Krzyszio <95748796+krzysztofmaron@users.noreply.github.com>
2026-05-23 17:21:43 +0200

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -96,7 +96,10 @@ DATABASES = {
         "USER": os.getenv('USER'),
         "PASSWORD": os.getenv('PASSWORD'),
         "HOST": os.getenv('HOST'),
-        "PORT": os.getenv('PORT')
+        "PORT": os.getenv('PORT'),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
